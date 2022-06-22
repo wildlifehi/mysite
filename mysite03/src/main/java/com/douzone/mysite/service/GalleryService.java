9f175a8409f661090b4f32203b4edd4822d82367
wildlifehi <wildlifehi@gamil.com>
2022-06-22 14:57:07 +0900

    commit

--- mysite03/src/main/java/com/douzone/mysite/service/GalleryService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/GalleryService.java
@@ -1,13 +1,16 @@
 package com.douzone.mysite.service;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.douzone.mysite.repository.GalleryRepository;
 import com.douzone.mysite.vo.GalleryVo;
 
 @Service
 public class GalleryService {
+	
 	@Autowired
 	private GalleryRepository galleryRepository;
 
