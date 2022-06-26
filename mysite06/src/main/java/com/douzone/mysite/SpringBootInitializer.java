e4077b78c92e2cd16f9ad4775c104630cd39ab34
wildlifehi <wildlifehi@gamil.com>
2022-06-26 15:58:56 +0900

    update

--- mysite06/src/main/java/com/douzone/mysite/SpringBootInitializer.java
+++ mysite06/src/main/java/com/douzone/mysite/SpringBootInitializer.java
@@ -4,12 +4,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class SpringBootInitializer extends SpringBootServletInitializer {
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(MySiteApplication.class);
-		
 	}
-	
-
 }
