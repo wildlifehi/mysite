a552d522baccea44054ef1137923740f9608a654
wildlifehi <wildlifehi@gamil.com>
2022-06-30 17:17:32 +0900

    update

--- mysite05/src/main/java/com/douzone/mysite/initializer/MySiteWebApplicationInitializer.java
+++ mysite05/src/main/java/com/douzone/mysite/initializer/MySiteWebApplicationInitializer.java
@@ -12,21 +12,26 @@ import com.douzone.mysite.config.app.AppConfig;
 import com.douzone.mysite.config.web.WebConfig;
 
 public class MySiteWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	// Root WebApplicationContext
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class<?>[] {AppConfig.class};
 	}
 
+	// Servlet WebApplicationContext
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class<?>[] {WebConfig.class};
 	}
 
+	// DispatcherServlet Mapping
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] {"/"};
 	}
 
+	// Filter 
 	@Override
 	protected Filter[] getServletFilters() {
 		return new Filter[] {new CharacterEncodingFilter("UTF-8")};
